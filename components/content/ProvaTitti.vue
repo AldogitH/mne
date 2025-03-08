cda4f4374ea379e6f288889d04c8e0d629513634
info@28do.it <info@28do.it>
2025-03-08 16:47:47 +0100

    Aggiunta del componente ProvaTitti con integrazione di Nuxt Content per il recupero dei testi

---
+++ components/content/ProvaTitti.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="provaComponenteTitti" class="element">
+    <slot name="titolo" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, nextTick } from "vue";
+onMounted(() => {
+  nextTick(async () => {
+    //richiamo i testi dei moduli con nuxt content
+    const { data: doc } = await useAsyncData(() =>
+      queryCollection("content").path("/testiprova").first()
+    );
+  }); //NOTE - end nextTick
+});
+</script>
+
+<style lang="scss" scoped></style>
